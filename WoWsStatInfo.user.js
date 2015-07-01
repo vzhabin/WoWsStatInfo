512359d83d25ce5fb6a4073f8a22392cffcfaee9
vzhabin <vzhabin@fors.ru>
2015-07-01 10:09:16 +0300

    0.4.0.10

--- WoWsStatInfo.user.js
+++ WoWsStatInfo.user.js
@@ -5,7 +5,7 @@
 // @copyright 2015+, Vov_chiK
 // @license GPL version 3 or any later version; http://www.gnu.org/copyleft/gpl.html
 // @namespace http://forum.walkure.pro/
-// @version 0.4.0.9
+// @version 0.4.0.10
 // @creator Vov_chiK
 // @include http://worldofwarships.ru/community/accounts/*
 // @include http://worldofwarships.eu/community/accounts/*
@@ -24,7 +24,7 @@
 (function(window){//http://eu.wargaming.net/clans/500015661/
 	/* ===== Main function ===== */
 	function WoWsStatInfo(){
-		var VersionWoWsStatInfo = '0.4.0.9';
+		var VersionWoWsStatInfo = '0.4.0.10';
 		
 		var WoWsStatInfoLinkLoc = [];
 		WoWsStatInfoLinkLoc['ru'] = 'http://forum.worldofwarships.ru/index.php?/topic/19158-';
